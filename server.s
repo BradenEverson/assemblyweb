44f1e4c19b099f2693217e9b3703c40dfea9ce24
BradenEverson <bradeneverson@gmail.com>
2025-02-21 20:27:12 -0600

    Add error logging, new error found in doing so

--- server.s
+++ server.s
@@ -5,15 +5,16 @@
     .text
 _start:
     # socket(2, 1, 0)
-    mov     $0x29, %rax             # system call 41 is socket
+    mov     $41, %rax             # system call 41 is socket
     mov     $2, %rdi                # AF_INET is 2
     mov     $1, %rsi                # SOCK_STREAM is 1
     mov     $0, %rdx                # Options set to 0
     syscall
 
-    mov     %rax, %rbx              # Put FD in rbx for safe keeping
+    test    %rax, %rax              # Check if rax is negative
+    js      error_handle            # jump to error handler if things go south
 
-    # TODO: If rax is -1 we should be scared
+    mov     %rax, %rbx              # Put FD in rbx for safe keeping
 
     # bind(rbx, sock_addr pointer?, len)
     mov     $49, %rax               # system call 49 is bind
@@ -22,11 +23,17 @@ _start:
     mov     $16, %rdx               # sizeof(struct sockaddr_in)
     syscall 
 
+    test    %rax, %rax              # Check if rax is negative
+    js      error_handle            # jump to error handler if things go south
+
     # listen (rbx, 4096)
     mov     $50, %rax               # system call 50 is listen
     mov     %rbx, %rdi              # socket fd 
     mov     $4096, %rsi             # max connections
     syscall
+
+    test    %rax, %rax              # Check if rax is negative
+    js      error_handle            # jump to error handler if things go south
 
 loop:
     # accept(rbx, sock_addr pointer, socklen_t *)
@@ -35,26 +42,36 @@ loop:
     lea addr_len(%rip), %rdx        # Pointer to socklen_t (size of sockaddr)
     syscall
 
+    test    %rax, %rax              # Check if rax is negative
+    js      error_handle            # jump to error handler if things go south
+
     mov     %rax, %rcx              # Put new FD in rcx for safe keeping
 
-    # write(rcx, response, 97)
+    # write(rcx, response, 89)
     mov     $1, %rax                # syscall 1 is write
     mov     %rcx, %rdi              # load new FD
     lea     response(%rip), %rsi    # response text
-    mov     $97, %rdx               # response length
+    mov     $89, %rdx               # response length
     syscall
+
+    test    %rax, %rax              # Check if rax is negative
+    js      error_handle            # jump to error handler if things go south
 
     # close(rcx)
     mov     $3, %rax                # system call 3 is close
     mov     %rcx, %rdi              # Fd is stored in rcx
     syscall
 
-
     # Infinite loop to keep accepting connections
-    xor     %rdx, %rdx
-    cmp     %rdx, %rdx
-    je      loop
+    jmp     loop
 
+error_handle:
+    # write(1, message, 13)
+    mov     $1, %rax                # system call 1 is write
+    mov     $1, %rdi                # file handle 1 is stdout
+    lea     bad(%rip), %rsi         # address of string to output
+    mov     $45, %rdx
+    syscall
 
     # exit(0)
     mov     $60, %rax               # system call 60 is exit
@@ -75,3 +92,5 @@ addr:
     .section .rodata
 response:
     .ascii "HTTP/1.0 200 OK\r\nServer: server\r\n Content-type: text/html\r\n\r\n<html>Hello from ASM!</html>" # 97 bytes btw
+bad:
+    .ascii "So sorry something quite awful has happened\n" # 45 bytes btw
