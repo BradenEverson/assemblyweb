f3a25d55113977b44ce97c725f58aca2c037dc50
BradenEverson <bradeneverson@gmail.com>
2025-02-22 05:55:38 -0600

    New -1 check

--- server.s
+++ server.s
@@ -4,6 +4,10 @@
 
     .text
 _start:
+    mov     $0, %rdx 
+    dec     %rdx
+    push    %rdx
+
     # socket(2, 1, 0)
     mov     $41, %rax             # system call 41 is socket
     mov     $2, %rdi                # AF_INET is 2
@@ -11,8 +15,10 @@ _start:
     mov     $0, %rdx                # Options set to 0
     syscall
 
-    test    %rax, %rax              # Check if rax is negative
-    js      error_handle            # jump to error handler if things go south
+    pop     %rdx
+    cmp     %rax, %rdx              # Check if rax is negative
+    push    %rdx
+    je      error_handle            # jump to error handler if things go south
 
     mov     %rax, %rbx              # Put FD in rbx for safe keeping
 
@@ -23,8 +29,10 @@ _start:
     mov     $16, %rdx               # sizeof(struct sockaddr_in)
     syscall 
 
-    test    %rax, %rax              # Check if rax is negative
-    js      error_handle            # jump to error handler if things go south
+    pop     %rdx
+    cmp     %rax, %rdx              # Check if rax is negative
+    push    %rdx
+    je      error_handle            # jump to error handler if things go south
 
     # listen (rbx, 4096)
     mov     $50, %rax               # system call 50 is listen
@@ -32,8 +40,10 @@ _start:
     mov     $4096, %rsi             # max connections
     syscall
 
-    test    %rax, %rax              # Check if rax is negative
-    js      error_handle            # jump to error handler if things go south
+    pop     %rdx
+    cmp     %rax, %rdx              # Check if rax is negative
+    push    %rdx
+    je      error_handle            # jump to error handler if things go south
 
 loop:
     # accept(rbx, sock_addr pointer, socklen_t *)
@@ -42,8 +52,10 @@ loop:
     lea addr_len(%rip), %rdx        # Pointer to socklen_t (size of sockaddr)
     syscall
 
-    test    %rax, %rax              # Check if rax is negative
-    js      error_handle            # jump to error handler if things go south
+    pop     %rdx
+    cmp     %rax, %rdx              # Check if rax is negative
+    push    %rdx
+    je      error_handle            # jump to error handler if things go south
 
     mov     %rax, %rcx              # Put new FD in rcx for safe keeping
 
@@ -54,8 +66,10 @@ loop:
     mov     $89, %rdx               # response length
     syscall
 
-    test    %rax, %rax              # Check if rax is negative
-    js      error_handle            # jump to error handler if things go south
+    pop     %rdx
+    cmp     %rax, %rdx              # Check if rax is negative
+    push    %rdx
+    je      error_handle            # jump to error handler if things go south
 
     # close(rcx)
     mov     $3, %rax                # system call 3 is close
